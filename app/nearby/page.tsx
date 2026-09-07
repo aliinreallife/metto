81e7f48ad5232f52f39e2158dd1457d5a007e053
Ali Rashidi <aliinreallifee@gmail.com>
2026-09-07 13:06:09 -0400

    assets: use so.png as social preview image (1731x909)

--- app/nearby/page.tsx
+++ app/nearby/page.tsx
@@ -20,8 +20,8 @@ export const metadata: Metadata = {
     images: [
       {
         url: "/socialprev.png",
-        width: 1734,
-        height: 907,
+        width: 1731,
+        height: 909,
         alt: "متو - مسیریاب مترو تهران",
         type: "image/png",
       },
